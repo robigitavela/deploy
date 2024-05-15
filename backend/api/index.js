7c08c1c7ee3721a1cfab85d9813d86fb726e8ad5
Robyschumi <robert.andreev@yahoo.com>
2024-05-15 23:55:58 +0200

    najnova

--- backend/api/index.js
+++ backend/api/index.js
@@ -2,3 +2,4 @@ const express = require("express");
 const app = express();
 app.get("/", (req, res) => res.send("ke ja bide rabotava"));
 app.listen(10000, () => console.log("Listening on port 10000!"));
+module.exports = app;
