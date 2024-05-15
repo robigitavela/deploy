1ae3add86dc0ab1e46b5135409437d36059c1ba3
Robyschumi <robert.andreev@yahoo.com>
2024-05-16 00:21:20 +0200

    povtorno

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 app.get("/", (req, res) => {
-	res.send(`this is environment variable: ${process.env.TEST}`);
+	res.send[`this is environment variable: ${process.env.TEST}`];
 });
 app.listen(10000, () => console.log("Listening on port 10000!"));
+module.exports = app;
