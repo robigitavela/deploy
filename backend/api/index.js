4c75ef46f835669ad2675d8bbb476214a9779ac5
Robyschumi <robert.andreev@yahoo.com>
2024-05-16 00:13:47 +0200

    ne mi e jasno

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
 app.get("/", (req, res) => {
-	res.send("This is hard");
+	res.send(`this is environment variable: ${process.env.TEST}`);
 });
 app.listen(10000, () => console.log("Listening on port 10000!"));
