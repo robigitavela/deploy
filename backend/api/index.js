fa058b4f76aaf2e5b986dac5287f146a5071b037
Robyschumi <robert.andreev@yahoo.com>
2024-05-16 00:33:58 +0200

    posledno

--- backend/api/index.js
+++ backend/api/index.js
@@ -5,14 +5,8 @@ app.get("/", (req, res) => {
 	res.send(`This is an environment variable: ${process.env.TEST}`);
 });
 
-app.get("/test", (req, res) => {
-	return res.send(`Hello ${req.query.name}`);
-});
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-	console.log(`Server is running on port ${PORT}`);
+res.listen(10000, () => {
+	console.log("Listening on port 10000");
 });
 
 module.exports = app;
