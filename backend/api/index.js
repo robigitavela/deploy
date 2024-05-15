054e40991903f9b6a1e87768be0942a30e59513f
Robyschumi <robert.andreev@yahoo.com>
2024-05-16 00:39:35 +0200

    stanuvam veke

--- backend/api/index.js
+++ backend/api/index.js
@@ -5,7 +5,7 @@ app.get("/", (req, res) => {
 	res.send(`This is an environment variable: ${process.env.TEST}`);
 });
 
-res.listen(10000, () => {
+app.listen(10000, () => {
 	console.log("Listening on port 10000");
 });
 
