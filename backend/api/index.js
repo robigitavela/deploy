6ed216741c7b510f8ab2627f0cdf4090df81c4a6
Robyschumi <robert.andreev@yahoo.com>
2024-05-16 00:03:11 +0200

    pak i pak

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
-app.get("/", (req, res) => res.send("ke ja bide rabotava"));
+app.get("/", (req, res) => {
+	res.send("This is hard");
+});
 app.listen(10000, () => console.log("Listening on port 10000!"));
 module.exports = app;
