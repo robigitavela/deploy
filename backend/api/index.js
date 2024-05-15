340c53681234413a9e56cba446feb9e85542f115
Robyschumi <robert.andreev@yahoo.com>
2024-05-16 00:29:01 +0200

    ova od Vangel

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,7 +1,18 @@
 const express = require("express");
 const app = express();
+
 app.get("/", (req, res) => {
-	res.send(`this is environment variable: ${process.env.TEST}`);
+	res.send(`This is an environment variable: ${process.env.TEST}`);
 });
-app.listen(10000, () => console.log("Listening on port 10000!"));
+
+app.get("/test", (req, res) => {
+	return res.send(`Hello ${req.query.name}`);
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+	console.log(`Server is running on port ${PORT}`);
+});
+
 module.exports = app;
