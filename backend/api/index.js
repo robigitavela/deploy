4f71b7999fd1c5a1ac6e2bcc61c9dfccae0f8453
Robyschumi <robert.andreev@yahoo.com>
2024-05-15 23:21:30 +0200

    initial commit

---
+++ backend/api/index.js
@@ -0,0 +1,4 @@
+const express = require("express");
+const app = express();
+app.get("/", (req, res) => res.send("ke ja bide rabotava"));
+app.listen(10000, () => console.log("Listening on port 10000!"));
